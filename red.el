b27a4d4fbb99b71ff406a5ab2b84c4f3a0864b8a
Joshua Cearley <joshua.cearley@gmail.com>
2015-10-09 10:12:22 -0500

    A very basic (hopefully usable) mode for Red.

---
+++ red.el
@@ -0,0 +1,138 @@
+
+;;; Special parts which require text highlights
+
+; symbols
+; "% * ** + - / // < << <= <> = == =? > >= >> >>> ? ?? NaN? a-an"
+
+(defconst red-words-regex
+  (concat "\\<"
+	  (regexp-opt-group
+	   (split-string
+	    "
+NaN? a-an about absolute acos action? add ail alias all and and~ any
+any-block? any-function? any-object? any-path? any-string? any-word?
+append arccosine arcsine arctangent arctangent2 as as-pair asin ask at
+atan atan2 attempt back bind bitset? block? body-of break byte!
+c-string! case catch cause-error cdecl char? charset clear comment
+complement complement? complete-from-path compose cond construct
+context continue copy cos cosine datatype? declare
+default-input-completer dehex difference divide do does e either
+empty? equal? error? eval-set-path even? exclude exit exp extend false
+fifth file? find first float! float32! float? forall foreach forever
+form fourth func function function? get get-path? get-word?
+greater-or-equal? greater? halt has hash? head head? help if in index?
+input insert integer! integer? intersect issue? keys-of last last-lf?
+length? lesser-or-equal? lesser? lit-path? lit-word? load local log-10
+log-2 log-e logic! logic? loop lowercase make map? max min modify
+modulo mold multiply native? negate negative? next none?  not
+not-equal? object object? odd? op? or or~ pad pair? paren? parse
+parse-trace path? percent? pick pointer! poke positive? power prin
+print probe put quit quit-return quote random read-input reduce
+refinement? reflect remainder remove repeat replace return reverse
+round routine? same? second select series? set set-buffer-history
+set-path? set-word? shift shift-left shift-logical shift-right sin
+sine skip sort source spec-of square-root stats strict-equal? string?
+struct! subtract swap switch tail tail? take tan tangent third throw
+to to-hex trim true try tuple? type? typeset? union unique unless
+unset? axis until uppercase url? value? values-of variadic vector?
+what while word? words-of xor xor~ zero?")
+	   t t)
+	  "\\>")
+  "Words which are defined by default in the Red programming language.")
+
+(defconst red-font-lock-keywords
+  `((,red-words-regex . font-lock-function-name-face))
+  "Font lock table for the Red programming language")
+
+;;; Syntax table.
+(defvar red-syntax-table
+  (let ((synTable (make-syntax-table)))
+    ;; Lisp style comments
+    (modify-syntax-entry ?\; "<" synTable)
+    (modify-syntax-entry ?\n ">" synTable)
+    ;; Dashes are part of identifiers, and thus words
+    (modify-syntax-entry ?- "w" synTable)
+    synTable
+    )
+  "Syntax table for the Red language.")
+
+;;; Standard intelligent comments.
+(defun red-comment-dwim (arg)
+  "`Do What I Mean' commenting for Red. Based on the `comment-dwim' function."
+  (interactive "*P")
+  (require 'newcomment)
+  (let ((comment-start ";") (comment-end ""))
+    (comment-dwim arg)))
+
+;;; Line indentation.
+(defvar red-indentation-amount 3
+  "How many cells of indentation are used in Red source code.")
+
+(defun red-get-indentation-for-line ()
+  "Figures out the proper indentation for the current line."
+  (save-excursion
+    (if (bobp)
+	0
+      (let ((indentation 0)
+	    (closers "[\])}]")
+	    (openers "[\[({]"))
+	;; dedent if closers are present
+	(let* ((bol (progn (beginning-of-line)
+			   (point)))
+	       (eol (progn (end-of-line)
+			   (point)))
+	       (open (how-many openers bol eol))
+	       (close (how-many closers bol eol))
+	       (diff (- open close)))
+	  (if (< diff 0)
+	      (setq indentation (- 0 red-indentation-amount))))
+	;; add previous line's indentation
+	(previous-line)
+	(setq indentation (+ indentation (current-indentation)))
+	;; indent if openers are present
+	(let* ((bol (progn (beginning-of-line)
+			   (point)))
+	       (eol (progn (end-of-line)
+			   (point)))
+	       (open (how-many openers bol eol))
+	       (close (how-many closers bol eol))
+	       (diff (- open close)))
+	  (if (> diff 0)
+	      (setq indentation (+ indentation red-indentation-amount))))
+	(max 0 indentation)))))
+
+(defun red-indent-line ()
+  "Indents the current line using Red's indentation rules."
+  (interactive "*")
+  (let* ((a (point))
+	 (b (progn (back-to-indentation) (point))))
+    (indent-line-to (red-get-indentation-for-line))
+    (back-to-indentation)
+    (forward-char (- a b))))
+
+(defun red-electric-insert-and-indent ()
+  "Automatically reindents the current line after inserting a character."
+  (interactive "*")
+  (self-insert-command 1)
+  (red-indent-line))
+
+;;; Mode definition.
+(define-derived-mode red-mode fundamental-mode
+  "Red "
+  "Major mode for editing source code in the Red or Red/System programming languages."
+  :syntax-table red-syntax-table
+
+  ;; install font lock settings
+  (setq font-lock-defaults '((red-font-lock-keywords)))
+
+  ;; map smart commenting
+  (define-key red-mode-map [remap comment-dwim] 'red-comment-dwim)
+
+  ;; map electric indentation
+  (define-key red-mode-map "]" 'red-electric-insert-and-indent)
+  (define-key red-mode-map ")" 'red-electric-insert-and-indent)
+  (define-key red-mode-map "}" 'red-electric-insert-and-indent)
+
+  ;; map our indenter
+  (make-local-variable indent-line-function)
+  (setq indent-line-function 'red-indent-line))
